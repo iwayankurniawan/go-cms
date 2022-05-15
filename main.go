fdf50651ecf977881ac5bbf4b5ebc21bd87a775d
iwayankurniawan <kurniawanadityawardana@gmail.com>
2022-05-15 22:03:44 +0200

    add skeleton

--- main.go
+++ main.go
@@ -1,16 +1,84 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
-type Content struct{
-	id int
-	text string
+type Content struct {
+	Id   int
+	Text string
+}
+
+func createContent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	page := vars["page"]
+
+	fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+}
+
+func readContent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+		return
+	}
+
+	if err := id; err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Product not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, p)
+}
+
+func deleteContent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	page := vars["page"]
+
+	fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+}
+
+func updateContent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	page := vars["page"]
+
+	fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
 
 func main() {
-	c := Content{id:1, text:"hello"}
-	fmt.Println(c.text)
-}
+	r := mux.NewRouter()
 
+	r.HandleFunc("/project", createContent).Methods("POST")
+	r.HandleFunc("/project/{id}", readContent).Methods("GET")
+	r.HandleFunc("/project/{id}", deleteContent).Methods("DELETE")
+	r.HandleFunc("/project/{id}", updateContent).Methods("PUT")
+
+	http.ListenAndServe(":80", r)
+}
